bca0316b88c54fdeef8cd4d0b14a8c701f1a6dc9
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-11 09:15:08 +0530

    Adding Pages for courses and nav

--- src/pages/Courses/Lts.jsx
+++ src/pages/Courses/Lts.jsx
@@ -1,14 +1,56 @@
 export default function Lts(){
     return (
-        <div className="mx-10">
-            <h1 className="text-6xl"> SEMI-PRIVATE AND PRIVATE SWIMMING LESSONS:</h1>
+        <div className="mx-10 flex-col items-center">
             <div>
+            <h1 className="text-5xl"> SEMI-PRIVATE AND PRIVATE SWIMMING LESSONS:</h1>
+
+            </div>
+            <div className="mx-5  bg-amber-50">
                 <p>(SHOULD BE CONFIRMED PRIOR OF JOINING)
                 While our core group swim classes are small, 
                 our private and semi-private swimming classes provide a more personalized experience. 
                 Private lessons are 60 minutes long and pair one swimmer with one instructor. Semi-private 
                 lessons also last 60 minutes, but two swimmers can join the same lesson, which makes these lessons 
                 a wonderful fit for siblings or friends who want to swim together. </p>
+            </div>
+            <div>
+               <h1 className="text-4xl" >
+               We will follow these basic skill progressions during each class:
+               </h1>
+            </div>
+            <div className="flex">
+                <div className="w-1/2">
+                    <ul>
+                        <li>
+                        Graduate Skills
+                        </li>
+                        <li>
+                            Full submersion underwater (always child-led, never forced)
+                        </li>
+                        <li>
+                            Breath holding and breath
+                        </li>
+                        <li>
+                            Graduate Skills
+                        </li>
+                        <li>
+                            Front kicking
+
+                        </li>
+                        <li>
+                            In-line kicking
+                        </li>
+                        <li>
+                            Paddle stroke
+                        </li>
+                        <li>
+                            Freestyle stroke with a “Pop-up” breath
+                        </li>
+                    </ul>
+                </div>
+                <div className="bg-amber-500 w-1/2">
+                    ckjsdkcsncsksaj
+                </div>
             </div>
             
         </div>
