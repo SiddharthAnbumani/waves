8cb1feda57565ebeff3083cb64092ead5fc86754
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-15 09:31:20 +0530

    not important

--- src/pages/Courses/Lts.jsx
+++ src/pages/Courses/Lts.jsx
@@ -8,7 +8,7 @@ export default function Lts(){
             <h1 className="text-5xl"> SEMI-PRIVATE AND PRIVATE SWIMMING LESSONS:</h1>
 
             </div>
-            <div className="mx-5  bg-amber-50">
+            <div className="mx-5  bg-amber-50 text-lg">
                 <p>(SHOULD BE CONFIRMED PRIOR OF JOINING)
                 While our core group swim classes are small, 
                 our private and semi-private swimming classes provide a more personalized experience. 
@@ -22,7 +22,7 @@ export default function Lts(){
                </h1>
             </div>
             <div className="flex">
-                <div className="w-1/2">
+                <div className="w-1/2 text-lg">
                     <ul>
                         <li>
                         Graduate Skills
@@ -105,13 +105,44 @@ export default function Lts(){
             </div>
             </div>
 <div>
-    <h1 className='text-5xl'>
+    <h1 className='text-4xl font-bold text-center'>
     Every swimmer who joins us will learn to swim at their own pace in a learning environment that emphasizes empowerment and skill retention.
     </h1>
 </div>
 
-<div>
-    <h1 className='text-5xl'>SWIMMING PROGRAMME FOR ADULTS</h1>
+<div className='my-10'>
+    <h1 className='text-5xl text-center'>SWIMMING PROGRAMME FOR ADULTS</h1>
+
+    <p className='mx-10 text-lg mt-5'>
+    We understand that everyone enjoys or gets the most out of our LEARN TO SWIM programme. 
+    That’s why we give you the option of a bespoke swimming membership for you to develop 
+    further.Swimming is great exercise, too. It lets you get a full-body workout without 
+    putting too much strain through your joints and you’ll burn calories and build muscle 
+    at the same time.With an Everyone Active swimming membership, you get complete access
+    to our swimming pool. Whether you want to pound the lanes to up your fitness, hone your technique, 
+    or just enjoy the water. The choice is yours and there aresessions to suit everybody.
+    </p>
+
+    <div className='flex justify-center my-10'>
+    <div className='flex flex-col'>
+        <button className='w-120 bg-blue-500 px-10 py-4 font-extrabold border-2 rounded-md text-center'>
+            ONE MONTH MEMBERSHIP
+        </button>
+        <button className='w-120 bg-white px-10 py-4 font-extrabold border-2 rounded-md text-center'>
+            THREE MONTHS MEMBERSHIP (QUARTERLY)
+        </button>
+        <button className='w-120 bg-blue-500 px-10 py-4 font-extrabold border-2 rounded-md text-center'>
+            SIX MONTHS MEMBERSHIP (HALF YEARLY)
+        </button>
+        <button className='w-120 bg-white px-10 py-4 font-extrabold border-2 rounded-md text-center'>
+            ANNUAL MEMBERSHIP
+        </button>
+        <button className='w-120 bg-blue-500 px-10 py-4 font-extrabold border-2 rounded-md text-center'>
+            MEMBERSHIP WITH COACH PROGRAMME
+        </button>
+    </div>
+</div>
+
 </div>
 
             </div>
