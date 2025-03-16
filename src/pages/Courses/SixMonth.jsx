7b3b1a2213d96bdd8fd88418e8615bb9bef5b3ea
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-16 13:00:17 +0530

    Adding Pages for courses and nav

--- src/pages/Courses/SixMonth.jsx
+++ src/pages/Courses/SixMonth.jsx
@@ -2,58 +2,46 @@ export default function SixMonth(){
     return (
         <div>
             <div>
-                <h1 className='text-5xl font-bold text-center'>Three Month Course </h1>
+                <h1 className='text-5xl font-bold text-center'>Six Month Course </h1>
             </div>
             <div className='flex'>
                 <div className='w-2/3 bg-blue-200'>
-                    <h1 className='text-2xl text-center font-black mt-5'>Duration of course – 3 month</h1>
+                    <h1 className='text-2xl text-center font-black mt-5'>Duration of course – 6 month</h1>
                     <p className='text-lg font-semibold mt-5'>
-                        In this class, children will learn how to swim the 
-                        strokes of Freestyle (with side breathing) 
-                        and Backstroke. Swim Strokes is drill-oriented and
-                        follows these skill progressions during each class:
+                        In this class, children will learn the advanced strokes of 
+                        Free style,Back stroke,Breaststroke and Butterfly. In addition, 
+                        children will refine and further develop their Freestyle and 
+                        Backstroke swimming. The lessons are drill and technique-oriented 
+                        and will follow these skill progressions during each class:
                     </p>
                     <ul className='mt-5 ml-5 font-semibold'>
                         <li>
-                            Further practice of breath control
+                            Freestyle & Backstroke (refinement purposes)  
                         </li>
                         <li>
-                            Flutter kicking with kickboard, and in streamline on tummy and back
+                            Breaststroke kick
                         </li>
                         <li>
-                            Stationary side breathing
+                            Breaststroke arm stroke, kick & stroke timing
                         </li>
                         <li>
-                            1-arm Freestyle with side breathing
+                            Body dolphin butterfly kick
                         </li>
                         <li>
-                            Freestyle with side breathing
+                            Butterfly arm stroke, kick &
                         </li>
                         <li>
-                            Back stroke
-                        </li>
-                        <li>
-                            Start from block
-                        </li>
-                        <li>
-                            Free style roll in water
+                            Stroke timing
                         </li>
                     </ul>
                     <h1 className='text-xl font-extrabold'>
                         Graduate Skills:
                     </h1>
                     <p className='text-lg font-semibold'>
-                    Swim 25 feet Freestyle, demonstrating at least 3 side breaths Swim 15 feet Backstroke on the surface of the water. Kick 25 M on the back, without a pool aid, with the arms at the sides. Kick 25M on the stomach in streamline, without a noodle.
+                    Swim 25 M of Free style, Back stroke, Breaststroke 
+                    and Butterfly “I.M. Special” Graduate: Swim a continuous 
+                    100M I.M. (individual medley)
                     </p>
-                    <span className='inline-flex'>
-                    <h1 className='text-xl font-extrabold'>
-                        NOTES:
-                    </h1>
-                    <p className='text-lg font-semibold ml-2'>
-                    graduating from this, you may choose to take 
-                    Advanced Swim Strokes or Strokes school..
-                    </p>
-                    </span>
                    
                 </div>
 
--- src/pages/Courses/StrokeSchool.jsx
+++ src/pages/Courses/StrokeSchool.jsx
@@ -1,5 +1,34 @@
 export default function StrokeSchool(){
     return (
-        <h1>This is StrokeSchool</h1>
+        <div>
+            <div>
+                <h1 className='text-5xl font-bold text-center'>Six Month Course </h1>
+            </div>
+            <div className='flex'>
+                <div className='w-2/3 bg-blue-200'>
+                    <h1 className='text-2xl text-center font-black mt-5'>Stroke School – 12 month</h1>
+                    <h1 className='text-xl font-extrabold'>
+                        Graduate Skills:
+                    </h1>
+                    <p className='text-lg font-semibold'>
+                        Graduate Skills:
+                        Our Stroke School program is an essential part of every swimmer’s journey to 
+                        become a well-rounded, proficient swimmer. In Stroke School practices, we are 
+                        teaching the sport of swimming, not just swimming up and down the pool. 
+                        Our Coaches take the time to teach swimmers the fundamentals of competitive 
+                        swimming while building a love for the sport through fun, engaging learning. 
+                        We teach, develop, and improve Freestyle, Backstroke, Breaststroke, Butterfly, 
+                        competitive skills like diving, flip turns, and touch turns, and focus on teaching 
+                        swimming etiquette and racing strategy. The small-group atmosphere in our Stroke School 
+                        Levels mean that our Coaches pay attention to every detail to help your swimmer become 
+                        the best they can be.
+                    </p>
+                </div>
+
+                <div className='w-1/3 bg-blue-300'>
+                    An Image will Come here
+                </div>
+            </div>
+        </div>
     )
 }
