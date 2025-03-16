2ecaf39c0819b7710f72b24fb2825087dcb817e3
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-16 12:46:32 +0530

    Adding Pages for courses and nav

--- src/pages/Courses/SixMonth.jsx
+++ src/pages/Courses/SixMonth.jsx
@@ -1,5 +1,66 @@
 export default function SixMonth(){
     return (
-        <h1>This is six Month</h1>
+        <div>
+            <div>
+                <h1 className='text-5xl font-bold text-center'>Three Month Course </h1>
+            </div>
+            <div className='flex'>
+                <div className='w-2/3 bg-blue-200'>
+                    <h1 className='text-2xl text-center font-black mt-5'>Duration of course – 3 month</h1>
+                    <p className='text-lg font-semibold mt-5'>
+                        In this class, children will learn how to swim the 
+                        strokes of Freestyle (with side breathing) 
+                        and Backstroke. Swim Strokes is drill-oriented and
+                        follows these skill progressions during each class:
+                    </p>
+                    <ul className='mt-5 ml-5 font-semibold'>
+                        <li>
+                            Further practice of breath control
+                        </li>
+                        <li>
+                            Flutter kicking with kickboard, and in streamline on tummy and back
+                        </li>
+                        <li>
+                            Stationary side breathing
+                        </li>
+                        <li>
+                            1-arm Freestyle with side breathing
+                        </li>
+                        <li>
+                            Freestyle with side breathing
+                        </li>
+                        <li>
+                            Back stroke
+                        </li>
+                        <li>
+                            Start from block
+                        </li>
+                        <li>
+                            Free style roll in water
+                        </li>
+                    </ul>
+                    <h1 className='text-xl font-extrabold'>
+                        Graduate Skills:
+                    </h1>
+                    <p className='text-lg font-semibold'>
+                    Swim 25 feet Freestyle, demonstrating at least 3 side breaths Swim 15 feet Backstroke on the surface of the water. Kick 25 M on the back, without a pool aid, with the arms at the sides. Kick 25M on the stomach in streamline, without a noodle.
+                    </p>
+                    <span className='inline-flex'>
+                    <h1 className='text-xl font-extrabold'>
+                        NOTES:
+                    </h1>
+                    <p className='text-lg font-semibold ml-2'>
+                    graduating from this, you may choose to take 
+                    Advanced Swim Strokes or Strokes school..
+                    </p>
+                    </span>
+                   
+                </div>
+
+                <div className='w-1/3 bg-blue-300'>
+                An Image will Come here
+                </div>
+            </div>
+        </div>
     )
 }
