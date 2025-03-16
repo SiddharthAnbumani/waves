82bac0638917819c7bf391e8acdffc0cfd489aa6
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-16 12:28:48 +0530

    NI

--- src/pages/Courses/OneMonth.jsx
+++ src/pages/Courses/OneMonth.jsx
@@ -7,16 +7,16 @@ export default function OneMonth(){
                 <h1 className='text-5xl font-bold text-center'>One Month Course</h1>
             </div>
             <div className='flex'>
-                <div className='w-2/3'>
-                    <h1 className='text-2xl'>Duration of course – 1 month</h1>
-                    <p className=''>
+                <div className='w-2/3 bg-blue-200'>
+                    <h1 className='text-2xl text-center font-black mt-5'>Duration of course – 1 month</h1>
+                    <p className='text-lg font-semibold mt-5'>
                         In this class, children will learn how to swim the strokes of 
                         Freestyle (with side breathing) and will be introduced to Backstroke. 
                         The Freestyle side breathing skill may be the most important skill a 
                         child learns in swim lessons. Swim Strokes is drill-oriented and follows 
                         these skill progressions during each class:
                     </p>
-                    <ul>
+                    <ul className='mt-5 ml-5 font-semibold'>
                         <li>
                             Further practice of breath control
                         </li>
@@ -32,7 +32,25 @@ export default function OneMonth(){
                         <li>
                             Float on Back – introduction to back st
                         </li>
+                        <li>
+                        Dive from block and swim freestyle
+                        </li>
                     </ul>
+                    <h1>
+Graduate Skills:
+                    </h1>
+                    <p>
+Swim 25 M Freestyle, demonstrating at least 3 side breaths, Swim 10 M Backstroke on the surface of the water. Kick 25M on the back,with a kick board, with the arms at the sides. Kick 10M on the stomach in streamline, without a kick board.
+
+                    </p>
+                    <h1>
+                    NOTES:
+
+                    </h1>
+                    <p>
+After graduating from this technique, you may choose to take Advanced Swim Strokes.
+
+                    </p>
                 </div>
 
                 <div className='w-1/3 bg-blue-300'>
@@ -42,5 +60,7 @@ export default function OneMonth(){
         </div>
     )
 }
+
+
 
 
