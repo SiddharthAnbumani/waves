4e28ed88fc66488cf3e6899132404c32c0ca0989
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-16 12:43:12 +0530

    Adding Pages for courses and nav

--- src/pages/Courses/ThreeMonth.jsx
+++ src/pages/Courses/ThreeMonth.jsx
@@ -2,17 +2,16 @@ export default function ThreeMonth(){
     return (
         <div>
             <div>
-                <h1 className='text-5xl font-bold text-center'>One Month Course</h1>
+                <h1 className='text-5xl font-bold text-center'>Three Month Course </h1>
             </div>
             <div className='flex'>
                 <div className='w-2/3 bg-blue-200'>
-                    <h1 className='text-2xl text-center font-black mt-5'>Duration of course – 1 month</h1>
+                    <h1 className='text-2xl text-center font-black mt-5'>Duration of course – 3 month</h1>
                     <p className='text-lg font-semibold mt-5'>
-                        In this class, children will learn how to swim the strokes of 
-                        Freestyle (with side breathing) and will be introduced to Backstroke. 
-                        The Freestyle side breathing skill may be the most important skill a 
-                        child learns in swim lessons. Swim Strokes is drill-oriented and follows 
-                        these skill progressions during each class:
+                        In this class, children will learn how to swim the 
+                        strokes of Freestyle (with side breathing) 
+                        and Backstroke. Swim Strokes is drill-oriented and
+                        follows these skill progressions during each class:
                     </p>
                     <ul className='mt-5 ml-5 font-semibold'>
                         <li>
@@ -25,31 +24,34 @@ export default function ThreeMonth(){
                             Stationary side breathing
                         </li>
                         <li>
+                            1-arm Freestyle with side breathing
+                        </li>
+                        <li>
                             Freestyle with side breathing
                         </li>
                         <li>
-                            Float on Back – introduction to back st
+                            Back stroke
                         </li>
                         <li>
-                        Dive from block and swim freestyle
+                            Start from block
+                        </li>
+                        <li>
+                            Free style roll in water
                         </li>
                     </ul>
                     <h1 className='text-xl font-extrabold'>
                         Graduate Skills:
                     </h1>
                     <p className='text-lg font-semibold'>
-                        Swim 25 M Freestyle, demonstrating at least 3 side breaths,
-                         Swim 10 M Backstroke on the surface of the water. Kick 25M 
-                         on the back,with a kick board, with the arms at the sides. 
-                         Kick 10M on the stomach in streamline, without a kick board.
+                    Swim 25 feet Freestyle, demonstrating at least 3 side breaths Swim 15 feet Backstroke on the surface of the water. Kick 25 M on the back, without a pool aid, with the arms at the sides. Kick 25M on the stomach in streamline, without a noodle.
                     </p>
                     <span className='inline-flex'>
                     <h1 className='text-xl font-extrabold'>
                         NOTES:
                     </h1>
                     <p className='text-lg font-semibold ml-2'>
-                        After graduating from this technique,
-                        you may choose to take Advanced Swim Strokes.
+                    graduating from this, you may choose to take 
+                    Advanced Swim Strokes or Strokes school..
                     </p>
                     </span>
                    
@@ -62,3 +64,4 @@ export default function ThreeMonth(){
         </div>
     )
 }
+
