e596bb1ce76b79f09511426f863498b8371d9ea7
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-19 10:55:27 +0530

    Adding Pages for courses and nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Carousel from '../components/Carousel'
+import NewHeader from '../components/NewHeader'
 import { Button } from 'react-bootstrap'
 
 export default function Home(){
   return (
     <div>
       <Button/>
+      <NewHeader background='Competitive.avif'/>
     </div>
   )
 }
